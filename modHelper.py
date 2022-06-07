ff74c6b5dcb020226271c82c745c961ab23e8ce8
franciumxzf <92449199+franciumxzf@users.noreply.github.com>
2022-06-08 02:58:10 +0800

    Update modHelper.py

--- modHelper.py
+++ modHelper.py
@@ -1,17 +1,7 @@
 import praw
+import pdb
+import re
+import os
 
 reddit = praw.Reddit('modHelper')
-subreddit = reddit.subreddit("pythonforengineers")
-
-#Access the post in a subreddit
-for submission in subreddit.hot(limit = 2):
-    print("Title: ", submission.title)
-    print("Text: ", submission.selftext)
-    print("Score: ", submission.score)
-    print("---------------------------------\n")
-
-#Access the comments in a subreddit
-subreddit_comments = subreddit.comments(limit = 10)
-for comment in subreddit_comments:
-    print(comment.id)
-    print(comment.body)
+subreddit = reddit.subreddit("nus")
