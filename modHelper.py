87043e19a4efd045c541b889f93d61ca0ecdda1a
Haoying0822 <106087753+Haoying0822@users.noreply.github.com>
2022-06-09 00:26:33 +0800

    Update modHelper.py
    updated with feature 1 established: the bot replies with tele links adapted from https://telenus.nusmods.com/ once bot is called in the comment section of top 20 posts sorted by new. The program is currently set to run every hour, yet the actual running frequency can be changed according to needs. 
    
    Note: r/pythonforengineers is a subreddit we are using for testing purposes as we do not want to spam and disturb r/NUS.

--- modHelper.py
+++ modHelper.py
@@ -1,7 +1,44 @@
+from time import sleep
 import praw
 import pdb
 import re
 import os
 
 reddit = praw.Reddit('modHelper')
-subreddit = reddit.subreddit("nus")
+subreddit = reddit.subreddit("pythonforengineers")
+
+while True:
+    
+    if not os.path.isfile("archive.txt"):
+        archive = []
+    else:
+        with open("archive.txt", "r") as f:
+            archive = f.read()
+            archive = archive.split("\n")
+            archive = list(filter(None, archive))
+
+    for sub in subreddit.new(limit = 50):
+        if sub.id not in archive:
+            for com in sub.comments:
+                if re.search("u/modHelper", com.body, re.IGNORECASE):
+                    if re.search("tele", com.body, re.IGNORECASE):
+                        module_code = com.body.split()[-1]
+                        #com.reply(body = module_code + "\n***\n" + "^(This action is performed by a bot)")
+                        com.reply(body = "https://telenus.nusmods.com/ ^(This action is performed by a bot)")
+
+
+                        
+                    '''elif re.search("mate", com.body, re.IGNORECASE):
+                        pass
+                    '''
+
+
+                    
+                    archive.append(sub.id)
+                
+
+    with open("archive.txt", "w") as f:
+        for post_id in archive:
+            f.write(post_id + "\n")
+
+    sleep(3600)
